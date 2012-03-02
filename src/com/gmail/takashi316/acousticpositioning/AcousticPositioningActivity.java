3668d9342cc4c744ee45666a1a8a17490d23f1b5
Takashi SASAKI <takashi316@gmail.com>
2012-03-02 18:31:18 +0900

    Eclipse project created.

---
+++ src/com/gmail/takashi316/acousticpositioning/AcousticPositioningActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.takashi316.acousticpositioning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcousticPositioningActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
