8b1e1021e8282d3cb3313f1df4d21c8e06a685b8
Takashi SASAKI <takashi316@gmail.com>
2012-03-02 21:23:40 +0900

    onStop() was overridden.

--- src/com/gmail/takashi316/acousticpositioning/AcousticPositioningActivity.java
+++ src/com/gmail/takashi316/acousticpositioning/AcousticPositioningActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 public class AcousticPositioningActivity extends Activity {
 	/** Called when the activity is first created. */
 	AudioTrack audioTrack;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		audioTrack = new SineAudioTrack(2000,60);
+		audioTrack = new SineAudioTrack(2000, 60);
 		audioTrack.play();
+	}// onCreate
+
+	@Override
+	public void onStop() {
+		audioTrack.release();
+		super.onStop();
 	}
-}
+}// AcousticPositioningActivity
