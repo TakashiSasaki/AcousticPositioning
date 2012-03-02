bfe8e964eb43138aeab82de201c4f2f15a589f9a
Takashi SASAKI <takashi316@gmail.com>
2012-03-02 19:35:54 +0900

    succeeded to play short beep.

--- src/com/gmail/takashi316/acousticpositioning/AcousticPositioningActivity.java
+++ src/com/gmail/takashi316/acousticpositioning/AcousticPositioningActivity.java
@@ -1,13 +1,33 @@
 package com.gmail.takashi316.acousticpositioning;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.provider.MediaStore.Audio;
 
 public class AcousticPositioningActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	private AudioTrack audioTrack;
+	private int minBufferSize = AudioTrack.getMinBufferSize(44100,
+			AudioFormat.CHANNEL_CONFIGURATION_MONO,
+			AudioFormat.ENCODING_PCM_8BIT);
+	private byte[] buffer = new byte[minBufferSize * 100];
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+				AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_8BIT, buffer.length,
+				AudioTrack.MODE_STATIC);
+		Log.v(new Throwable(), "bufferSize = " + buffer.length);
+
+		SignAudio sign_audio = new SignAudio(44100, 2000);
+		sign_audio.writeBuffer(buffer);
+		audioTrack.write(this.buffer, 0, buffer.length);
+		audioTrack.play();
+	}
 }
---
+++ src/com/gmail/takashi316/acousticpositioning/SignAudio.java
@@ -0,0 +1,23 @@
+package com.gmail.takashi316.acousticpositioning;
+
+public class SignAudio {
+	private int samplingRate;
+	private int frequency;
+
+	public SignAudio(int sampling_rate, int frequency) {
+		this.samplingRate = sampling_rate;
+		this.frequency = frequency;
+	}
+
+	public void writeBuffer(byte[] buffer) {
+		for (int i = 0; i < buffer.length; ++i) {
+			double t = (double) i * (1.0d / (double)samplingRate);
+			double signal = Math.sin(2.0d * Math.PI * (double)this.frequency * t) * 127.0d;
+			byte byte_signal = (byte) signal;
+			buffer[i] = byte_signal;
+		}// for
+	}
+
+	public void writeBuffer(short[] buffer) {
+	}
+}
