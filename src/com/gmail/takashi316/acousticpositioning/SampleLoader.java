c66771ef0e5a50da37684a4db0dec18f909bb3d3
unknown <Takashi@vpcz22.(none)>
2012-03-11 11:17:20 +0900

    SampleLoader loads short values in a file per line.

---
+++ src/com/gmail/takashi316/acousticpositioning/SampleLoader.java
@@ -0,0 +1,39 @@
+package com.gmail.takashi316.acousticpositioning;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SampleLoader {
+	BufferedReader bufferedReader;
+
+	public SampleLoader(String file_name) throws IOException {
+		ReaderThread rt = new ReaderThread(file_name);
+		this.bufferedReader = rt.getBufferedReader();
+	}
+
+	public short[] load() throws IOException {
+		ArrayList<Short> array_list = new ArrayList<Short>();
+		String line;
+		while ((line = this.bufferedReader.readLine()) != null) {
+			array_list.add(Short.parseShort(line));
+		}// while
+		short[] primitive_array = new short[array_list.size()];
+		int i = 0;
+		for (Short s : array_list) {
+			primitive_array[i] = s.shortValue();
+			++i;
+		}// for
+		return primitive_array;
+	}// load
+
+	public void load(short[] primitive_array) throws NumberFormatException,
+			IOException {
+		String line;
+		int i = 0;
+		while ((line = this.bufferedReader.readLine()) != null) {
+			primitive_array[i] = Short.parseShort(line);
+			++i;
+		}// while
+	}
+}
